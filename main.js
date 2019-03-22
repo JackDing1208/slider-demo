babfb82a8d3cef59a7e097e0d2344dcedea14053
JackDing <361164217@qq.com>
2019-03-22 20:07:42 +0800

    自动轮播

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-let $allspan=$('span')
-
-for(let i=0;i<$allspan.length;i++){
+let $allspan = $('span')
+//手动轮播
+for (let i = 0; i < $allspan.length; i++) {
     $($allspan[i]).click(function (x) {
-        let n=$(x.target).index()
-        $($allspan[i]).addClass('red')
-        $($allspan[i]).siblings().removeClass('red')
-        let p=n*100
+        let index = $(x.target).index()
+        $allspan.eq(i).addClass('red')
+        $allspan.eq(i).siblings().removeClass('red')
+        let p = index * 100
         $('.image').css({
-            transform: 'translateX(-'+p+'%)'
+            transform: 'translateX(-' + p + '%)'
         })
+        n = index
     })
 }
+
+//自动轮播
+var n = 0
+var size = $allspan.length
+$allspan.eq(n % size).trigger('click')
+var timer1 = setInterval(() => {
+    n += 1
+    $allspan.eq(n % size).trigger('click')
+}, 2000)
+
+//
+$('.window').mouseenter(function () {
+    window.clearInterval(timer1)
+})
+$('.window').mouseleave(function () {
+    timer1 = setInterval(() => {
+        n += 1
+        $allspan.eq(n % size).trigger('click')
+    }, 2000)
+})
