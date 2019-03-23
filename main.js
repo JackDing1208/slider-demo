480f898357b7f558285df2f66b4f615e5c4a8bb5
JackDing <361164217@qq.com>
2019-03-23 18:19:36 +0800

    代码重构

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
-let $allspan = $('span')
+//初始化
+var $allspan = $('span')
+var n = 0
+var size = $allspan.length
+$allspan.eq(n % size).trigger('click')
+开始轮播()
 //手动轮播
+
 for (let i = 0; i < $allspan.length; i++) {
-    $($allspan[i]).click(function (x) {
+    手动轮播($allspan[i])
+}
+
+//模式切换
+$('.window').mouseenter(function () {
+    window.clearInterval(timer1)
+})
+$('.window').mouseleave(function () {
+    开始轮播()
+})
+
+
+
+
+
+
+
+
+
+//下面不用看
+function 开始轮播() {
+    timer1 = setInterval(() => {
+        n += 1
+        $allspan.eq(n % size).trigger('click')
+    }, 2000)
+}
+
+function 手动轮播(node) {
+    $(node).click(function (x) {
         let index = $(x.target).index()
-        $allspan.eq(i).addClass('red')
-        $allspan.eq(i).siblings().removeClass('red')
+        $(node).addClass('red')
+        $(node).siblings().removeClass('red')
         let p = index * 100
         $('.image').css({
             transform: 'translateX(-' + p + '%)'
@@ -12,23 +46,3 @@ for (let i = 0; i < $allspan.length; i++) {
         n = index
     })
 }
-
-//自动轮播
-var n = 0
-var size = $allspan.length
-$allspan.eq(n % size).trigger('click')
-var timer1 = setInterval(() => {
-    n += 1
-    $allspan.eq(n % size).trigger('click')
-}, 2000)
-
-//
-$('.window').mouseenter(function () {
-    window.clearInterval(timer1)
-})
-$('.window').mouseleave(function () {
-    timer1 = setInterval(() => {
-        n += 1
-        $allspan.eq(n % size).trigger('click')
-    }, 2000)
-})
